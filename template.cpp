b58f5f6ed8c0d1933d03a1b2c4231f24e122c022
Anton Bakhtin <7470747+akhti@users.noreply.github.com>
2019-03-03 12:14:40 -0500

    Update include list to work with clang

--- template.cpp
+++ template.cpp
@@ -1,4 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <queue>
+#include <bitset>
 
 // ############ TASK $$PROBLEM_LETTER$$ ##################
 
